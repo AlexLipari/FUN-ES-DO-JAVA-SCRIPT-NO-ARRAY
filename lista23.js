ce27f6cecbf4e1fa8742b635e7e3da09f6bad34b
Alexandre Lipari <santuslipari@gmail.com>
2022-09-13 11:37:32 -0300

    Função array. sort()

--- lista23.js
+++ lista23.js
@@ -8,12 +8,22 @@
 //     })
 //     console.log(resultado)
 // }
-let ID = [1, 4, 10, 40, 30]
-ExcluirId(ID)
-function ExcluirId(idParametro) {
-    let resultado = ID.some(function (idParametro) {
-        return (idParametro = !  2 == 0)
+// let ID = [1, 4, 10, 40, 30]
+// ExcluirId(ID)
+// function ExcluirId(idParametro) {
+//     let resultado = ID.some(function (idParametro) {
+//         return (idParametro % 2!= 0)
         
-    })
-    console.log(resultado)
+//     })
+//     console.log(resultado)
+// }
+
+//array . sort() ordenar
+let numeros = [8,2,5,1,9,100,4,3,7,6,0]
+ordenar(numeros)
+function ordenar(numerosParametro){
+numerosParametro = numeros.sort((a,b)=>{
+    return a-b
+})
+console.log(numeros)
 }
