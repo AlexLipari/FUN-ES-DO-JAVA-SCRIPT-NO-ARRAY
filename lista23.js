09f967177a85bc0b165baa6b0c056bb4033d771e
Alexandre Lipari <santuslipari@gmail.com>
2022-09-13 12:06:57 -0300

    Complemento Função array.sort com string.

--- lista23.js
+++ lista23.js
@@ -4,7 +4,7 @@
 // function ExcluirId(idParametro) {
 //     let resultado = ID.every(function (idParametro) {
 //         return (idParametro % 2 == 0)
-        
+
 //     })
 //     console.log(resultado)
 // }
@@ -13,17 +13,21 @@
 // function ExcluirId(idParametro) {
 //     let resultado = ID.some(function (idParametro) {
 //         return (idParametro % 2!= 0)
-        
+
 //     })
 //     console.log(resultado)
 // }
 
 //array . sort() ordenar
-let numeros = [8,2,5,1,9,100,4,3,7,6,0]
-ordenar(numeros)
-function ordenar(numerosParametro){
-numerosParametro = numeros.sort((a,b)=>{
-    return a-b
-})
-console.log(numeros)
+let numeros = [8, 2, 5, 1, 9, 100, 4, 3, 7, 6, 0]
+let nomes = ["Ligia", "cris", "bianca", "Sara", "Fabiana", "Eliane"]
+ordenar(numeros, nomes)
+function ordenar(numerosParametro, nomeParametro) {
+    numerosParametro = numeros.sort((a, b) => {
+        return a - b
+    })
+    nomeParametro = nomes.sort((a, b) => {
+        return a.localeCompare(b)
+    })
+    console.log(numeros, nomes)
 }
